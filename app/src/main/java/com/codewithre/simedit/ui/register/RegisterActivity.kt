a16b09c501d8af14ba9e02257312baca220399a0
gochujjang <41176663+gochujjang@users.noreply.github.com>
2024-06-15 14:50:11 +0700

    Slicing register and main (card)

--- app/src/main/java/com/codewithre/simedit/ui/register/RegisterActivity.kt
+++ app/src/main/java/com/codewithre/simedit/ui/register/RegisterActivity.kt
@@ -1,38 +1,30 @@
-package com.codewithre.simedit.ui.login
+package com.codewithre.simedit.ui.register
 
-import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.codewithre.simedit.R
-import com.codewithre.simedit.databinding.ActivityLoginBinding
-import com.codewithre.simedit.ui.main.MainActivity
-import com.codewithre.simedit.ui.register.RegisterActivity
+import com.codewithre.simedit.databinding.ActivityRegisterBinding
 
-class LoginActivity : AppCompatActivity() {
+class RegisterActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivityLoginBinding
+    private lateinit var binding: ActivityRegisterBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        binding = ActivityLoginBinding.inflate(layoutInflater)
+        binding = ActivityRegisterBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
 
-        binding.tvRegister.setOnClickListener {
-            val intent = Intent(this, RegisterActivity::class.java)
-            startActivity(intent)
-        }
-
-        binding.btnLogin.setOnClickListener {
-            val intent = Intent(this, MainActivity::class.java)
-            startActivity(intent)
+        binding.tvLogin.setOnClickListener {
             finish()
         }
     }
